28a869c9f8b0c93e9718781897951a1e36b9d982
sashaknw <kaniewa.aleksandra@gmail.com>
2025-02-04 15:22:18 +0000

    done

---
+++ src/components/ProfileImage.jsx
@@ -0,0 +1,13 @@
+
+ 
+ function ProfileImage({ image }) {
+    return (
+      <img
+        className="profile"
+        src={image}
+        alt="profile"
+      />
+    );
+  }
+  
+  export default ProfileImage
